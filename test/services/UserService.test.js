ce54c33e8c4206b5373c92ed2ddc0a6b900eaa10
Joel Trinidad <jth_2929@hotmail.com>
2022-04-25 17:57:13 -0500

    test for getInfo UserService function

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -3,11 +3,21 @@ const UserService = require('./../../app/services/UserService');
 describe("Test for UserService",() => {
 
     test("1) Create a new user using the UserService", () => {
-        const user = UserService.create(1, "carlogilmar", "Carlo")
+        const user = UserService.create(1, "carlogilmar", "Carlo");
         
         expect(user.id).toBe(1)
         expect(user.username).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
         expect(user.bio).not.toBeUndefined()
+    });
+    
+    test("2) Get all user data in a list", () => {
+        const user = UserService.create(1, "carlogilmar", "Carlo");
+        const userInfoInList = UserService.getInfo(user);
+
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("carlogilmar")
+        expect(userInfoInList[2]).toBe("Carlo")
+        expect(userInfoInList[3]).toBe("Sin bio")
     });
 })
