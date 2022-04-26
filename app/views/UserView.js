23b9a49e65e2f16315440af649d9f0e8da3e2cdb
Joel Trinidad <jth_2929@hotmail.com>
2022-04-25 19:18:57 -0500

    UserView created and createUser function added to UserView

---
+++ app/views/UserView.js
@@ -0,0 +1,10 @@
+class UserView{
+    static createUser(payload){
+        if (payload) {
+            
+        }
+        return {error: 'payload no existe'}
+    }
+}
+
+module.exports = UserView;
