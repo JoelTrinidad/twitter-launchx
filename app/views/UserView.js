1ad5871906f0a54cc609bca03f454278da1a1ee7
Joel Trinidad <jth_2929@hotmail.com>
2022-04-25 20:00:52 -0500

    ·test to validate that playload properties aren't missing

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -2,7 +2,7 @@ class UserView{
     static createUser(payload){
         if (!payload) {
             return {error: 'payload no existe'};
-        }else if(payload.username == null || payload.username == null || payload.id == null){
+        }else if(!payload.username || !payload.username || !payload.id){
             return {error: 'necesitan tener un valor válido'};
         }
         
